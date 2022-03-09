cb459fe547e8c209bb2fa110c56f022ff631a5c1
Frederico J. R. Pelogia <fredpelogia@outlook.com>
2022-03-08 23:31:00 -0300

    Melhora gráficos e testa filtros

--- new_wave.py
+++ new_wave.py
@@ -40,14 +40,12 @@ def median_filter(x, win_size):
     strided = np.lib.stride_tricks.as_strided(x, shape=(nrows,win_size), strides=(S*n,n))
     return np.median(strided,axis=1)
 
-def butter_lowpass(cutoff, fs, order=5):
+def butterworth_lowpass_filter(data, cutoff_freq, fs, order=2):
+    #fs is the sampling rate
     nyq = 0.5 * fs
-    normal_cutoff = cutoff / nyq
+    normal_cutoff = cutoff_freq / nyq
     b, a = butter(order, normal_cutoff, btype='low', analog=False)
-    return b, a
-
-def butter_lowpass_filter(data, cutoff, fs, order=5):
-    b, a = butter_lowpass(cutoff, fs, order=order)
+    # lfilter apply filter along one dimension
     y = lfilter(b, a, data)
     return y
 
@@ -57,11 +55,10 @@ def filter_data(data):
     
     # 2nd Order Low-Pass Filter with 14-day window
     # 0.7 damping coefficient
-    # Setting standard filter requirements.
     order = 2
-    fs = len(data)       
-    cutoff = 14
-    filtered_data =  butter_lowpass_filter(filtered_data, cutoff, fs, order)
+    fs = len(data) # sampling rate       
+    cutoff = 14 # cuttof freq.
+    filtered_data =  butterworth_lowpass_filter(filtered_data, cutoff, fs, order)
 
     # Median filter with 14-day window
     filtered_data = median_filter(filtered_data, 14)
@@ -77,28 +74,27 @@ def forward_euler(t, step, y, dy0):
     return dy
 
 # New epidemiological wave detection
-#   Receives (sec_der, abs_treshold)
+#   Receives (sec_der, abs_threshold)
 #      sec_der: second derivative of acc. number of cases 
 #      abs_threshold: threshold to consider around zero
 #   Returns (x_t, y_t) -> coordinates of the transition points 
-def new_wave_detection(sec_der, abs_treshold):
+def new_wave_detection(sec_der, abs_threshold):
     x_t = []
     y_t = []
     for i in range(len(sec_der)-1):
-        if((sec_der[i] < abs_treshold) and (sec_der[i+1] > abs_treshold)):
+        if((sec_der[i] < abs_threshold) and (sec_der[i+1] > abs_threshold)):
            x_t.append(i+1)
            y_t.append(sec_der[i+1])
     return x_t, y_t
 
 def main():    
-
     #import data
     data = pd.read_csv("./Datasets/jerusalem.csv")  
 
     # Filter data to reduce noise effects
     # Normalize by maximum value
     normalized_acc_n_cases = data.total_confirmed / max(data.total_confirmed)
-    
+
 
     t = np.linspace(0, len(normalized_acc_n_cases), len(normalized_acc_n_cases))
     daily_n_cases = forward_euler(t, 1, normalized_acc_n_cases, 0)
@@ -106,14 +102,14 @@ def main():
     fig, axs = plt.subplots(1, 2, figsize=(15,5)) # 1 row, 2 cols
     axs[0].set_title('Numerical first derivative')
     axs[0].plot(daily_n_cases)
-    axs[0].set_xlabel('t')
+    axs[0].set_xlabel('t (days)')
     axs[0].set_ylabel('daily number of cases')
 
     daily_n_cases = filter_data(daily_n_cases)
 
     axs[1].set_title('Filtered numerical first derivative')
     axs[1].plot(daily_n_cases)
-    axs[1].set_xlabel('t')
+    axs[1].set_xlabel('t (days)')
     axs[1].set_ylabel('daily number of cases')
     plt.show()
 
@@ -124,33 +120,40 @@ def main():
     sec_der = forward_euler(t, 1, daily_n_cases, sd0)
 
     # Detection of new waves
-    abs_treshold = 3e-5
-    x_t, y_t = new_wave_detection(sec_der, abs_treshold)
+    abs_threshold = 3e-5
+    x_t, y_t = new_wave_detection(sec_der, abs_threshold)
 
     pylab.ticklabel_format(axis='y',style='sci',scilimits=(-4,-4))
     plt.title("Second derivative - New wave detection")
+    plt.xlabel("t (days)")
+    plt.ylabel("$cases$ / $day^2$")
     plt.plot(sec_der, zorder=1) # obs: check if this scaling is correct
-    plt.hlines([-1*abs_treshold, abs_treshold], 0, len(sec_der), colors='silver', linestyles='dashed', zorder=1, label=f"treshold = $\pm${abs_treshold}")
+    plt.hlines([-1*abs_threshold, abs_threshold], 0, len(sec_der), colors='silver', linestyles='dashed', zorder=1, label=f"threshold = $\pm${abs_threshold}")
     plt.vlines(x_t, -3e-4, 3e-4, colors='dimgray', linestyles='dashdot', zorder=1, label="new wave transition")
     plt.scatter(x_t, y_t, s=15, c='r', zorder=2, label="sign change in the second derivative")
     plt.legend()
     plt.show()
 
     # Graph with acc. data and its first two derivatives
-    fig, axs = plt.subplots(3, 1, figsize=(6,7)) # 3 rows, 1 col
+    fig, axs = plt.subplots(3, 1, figsize=(7,7)) # 3 rows, 1 col
     plt.tight_layout(pad=1.5)
-    axs[0].plot(normalized_acc_n_cases)
+    axs[0].plot(normalized_acc_n_cases[:len(daily_n_cases)]) # para alinhar as retas de nova onda
     axs[0].vlines(x_t, 1, 3e-4, colors='dimgray', linestyles='dashdot', zorder=1, label="new wave transition")
     axs[0].set_title('Normalized accumulated number of cases')
+    axs[0].set_ylabel("$cases$")
 
     axs[1].plot(daily_n_cases)
+    axs[1].ticklabel_format(axis='y',style='sci',scilimits=(-2,-2))
     axs[1].vlines(x_t, min(daily_n_cases), max(daily_n_cases), colors='dimgray', linestyles='dashdot', zorder=1, label="new wave transition")
     axs[1].set_title('First derivative')
+    axs[1].set_ylabel("$cases$ / $day$")
 
     axs[2].ticklabel_format(axis='y',style='sci',scilimits=(-4,-4))
     axs[2].set_title("Second derivative - New wave detection")
+    axs[2].set_xlabel("t (days)")
+    axs[2].set_ylabel("$cases$ / $day^2$")
     axs[2].plot(sec_der, zorder=1) # obs: check if this scaling is correct
-    axs[2].hlines([-1*abs_treshold, abs_treshold], 0, len(sec_der), colors='silver', linestyles='dashed', zorder=1, label=f"treshold = $\pm${abs_treshold}")
+    axs[2].hlines([-1*abs_threshold, abs_threshold], 0, len(sec_der), colors='silver', linestyles='dashed', zorder=1, label=f"threshold = $\pm${abs_threshold}")
     axs[2].vlines(x_t, -3e-4, 3e-4, colors='dimgray', linestyles='dashdot', zorder=1, label="new wave transition")
     axs[2].scatter(x_t, y_t, s=15, c='r', zorder=2, label="sign change in the second derivative")
     plt.show()
