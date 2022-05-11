6c072e0ee2f2d063a16e6820e883f302f618ac98
Frederico J. R. Pelogia <fredpelogia@outlook.com>
2022-05-10 22:31:01 -0300

    Começa a gerar resultados para Argentina

--- extract_data_deaths.py
+++ extract_data_deaths.py
@@ -2,14 +2,17 @@ import pandas as pd
 
 def main():
     # open dataset
-    data = pd.read_csv("../../Argentina/obitos_argentina_cidades.csv")
 
+    # Rosario:
+    #data = pd.read_csv("../../Argentina/obitos_argentina_cidades.csv")
+    # City of Buenos Aires, Mendoza, Córdoba
+    data = pd.read_csv("../../Argentina/obitos_argentina_estados.csv")
     # filter data from specific city
-    df = data[data.cidade == 'Rosario']
+    df = data[data.estado == 'Córdoba']
 
     cols_to_keep = [
         'date', 
-        'cidade', 
+        'estado',  # cidade (Rosario)
         'new_deceased',
         'cumulative_deceased'
     ]
@@ -18,7 +21,7 @@ def main():
     df.drop(df.columns.difference(cols_to_keep), 1, inplace=True)
 
     # Save reduced dataset
-    df.to_csv("Datasets/rosario_obitos.csv")
+    df.to_csv("Datasets/cordoba_obitos.csv")
 
     print("\nSuccessful Extraction!")
 
